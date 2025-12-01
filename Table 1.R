78baae9bfef651bd0ed100ba2aff203e3db242d0
FangYH130 <fangyh@ninh.chinacdc.cn>
2025-12-01 15:35:39 +0800

    Update Table 1.R

--- Table 1.R
+++ Table 1.R
@@ -2,9 +2,8 @@ library(haven)
 library(dplyr)
 library(broom)
 
-data_path <- "E:/2. 项目工作/1. 东方膳食模式评分/10分制/2025.8.19 补充分析/compare_20251113.sas7bdat"
+data_path <- "E:/score.sas7bdat"
 data <- read_sas(data_path)
-
 
 data <- data %>%
   mutate(overweight = factor(overweight, levels = c("BMI<24", "BMI≥24")),
@@ -55,4 +54,5 @@ model <- glm(formula = hp ~ scorea + A1 + age + ur_gt,
              family = binomial(link = "logit"))
 or_results <- tidy(model, conf.int = TRUE, exponentiate = TRUE)
 or_results
+
 
